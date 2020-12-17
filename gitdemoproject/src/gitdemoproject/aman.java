467d4b2e06f06258508c6800b424878362161572
Dell <Dell@DESKTOP-ANOO55L>
2020-12-17 17:07:42 +0530

    third commit

--- gitdemoproject/src/gitdemoproject/aman.java
+++ gitdemoproject/src/gitdemoproject/aman.java
@@ -1,6 +1,11 @@
 package gitdemoproject;
 
 public class aman {
+	
+	
+	public static void method1() {
+		System.out.println("second chage done and spoing new tyhing");
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
