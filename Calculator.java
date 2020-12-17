986d60b75050ebb798b4234372adc63480e9622a
aman agrawal <43132444+amangoyalji@users.noreply.github.com>
2020-12-17 03:14:30 -0500

    added multiply method
    multiplication method is added

--- Calculator.java
+++ Calculator.java
@@ -3,8 +3,13 @@ class  Calculator{
   public static int add(int num1,int num2){
     return (num1+num2);  
   }
+  
+  
+  public static int multiply(int num1,int num2){
+  return (num1*num2);
 
   public static void main(String srgs[]){
     System.out.println(add(10,20));
+    System.out.println(multiply(50*50));
   }
 }
