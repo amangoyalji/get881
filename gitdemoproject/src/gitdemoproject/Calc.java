4aa19425ebf709baaedab43540859951f0271582
amangoyalji <amana6398@gmail.com>
2020-12-17 17:46:16 +0530

    last commit  and added sub method for calc class from branch 1

--- gitdemoproject/src/gitdemoproject/Calc.java
+++ gitdemoproject/src/gitdemoproject/Calc.java
@@ -5,4 +5,12 @@ public class Calc {
 	public void add(int num1,int num2) {
 		System.out.println("sum ="+(num1+num2));
 	}
+	
+	
+	public void sub(int num1,int num2) {
+		System.out.println("sub ="+(num1-num2));
+	}
+	
+	
+	
 }
